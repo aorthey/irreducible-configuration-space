d3ea36d3bee479f6c0632ab6d42d0460d092ccc6
Nassime BLIN <nblin@laas.fr>
2014-05-19 14:19:56 +0200

    adding static script

---
+++ scripts/static_projection.py
@@ -0,0 +1,59 @@
+#/usr/bin/env python
+
+from hpp_ros import ScenePublisher
+from hpp.tools import PathPlayer
+from hpp.corbaserver.hrp2 import Robot
+from hpp.corbaserver.wholebody_step.client import Client as WsClient
+import time
+
+Robot.urdfSuffix = '_capsule'
+Robot.srdfSuffix= '_capsule'
+
+robot = Robot ('hrp2_14')
+robot.setTranslationBounds (-3, 3, -3, 3, 0, 1)
+cl = robot.client
+
+r = ScenePublisher (robot.jointNames [4:])
+q0 = robot.getInitialConfig ()
+r (q0)
+
+# Add constraints
+wcl = WsClient ()
+wcl.problem.addStaticStabilityConstraints ("balance", q0, robot.leftAnkle,
+                                           robot.rightAnkle)
+cl.problem.setNumericalConstraints ("balance", ["balance/relative-com",
+                                                "balance/relative-orientation",
+                                                "balance/relative-position",
+                                                "balance/orientation-left-foot",
+                                                "balance/position-left-foot"])
+
+# lock hands in closed position
+lockedDofs = robot.leftHandClosed ()
+for name, value in lockedDofs.iteritems ():
+    cl.problem.lockDof (name, value)
+
+lockedDofs = robot.rightHandClosed ()
+for name, value in lockedDofs.iteritems ():
+    cl.problem.lockDof (name, value)
+
+
+q1 = [0.0, 0.0, 0.705, 1.0, 0., 0., 0.0, 0.0, 0.0, 0.0, 0.0, -0.4, 0, -1.2, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
+
+q1proj = cl.problem.applyConstraints (q1)
+
+q2 = [0.0, 0.0, 0.705, 1, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, -1.4, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
+
+q2proj = cl.problem.createPositionConstraints (q2)
+#cl.problem.setInitialConfig (q1proj)
+#cl.problem.addGoalConfig (q2proj)
+#cl.problem.solve ()
+
+r(q2proj)
+
+time.sleep(1)
+
+
+r(q2proj)
+
+#p = PathPlayer (cl, r)
+#p (1)
