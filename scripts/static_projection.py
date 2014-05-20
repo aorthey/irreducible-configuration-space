2a16c838d1eff530d6b16a13f5420cbcf2563964
Nassime BLIN <nblin@laas.fr>
2014-05-20 10:52:39 +0200

    allow command line entry for static projection script

--- scripts/static_projection.py
+++ scripts/static_projection.py
@@ -5,6 +5,8 @@ from hpp.tools import PathPlayer
 from hpp.corbaserver.hrp2 import Robot
 from hpp.corbaserver.wholebody_step.client import Client as WsClient
 import time
+import sys
+
 
 Robot.urdfSuffix = '_capsule'
 Robot.srdfSuffix= '_capsule'
@@ -43,15 +45,26 @@ q1proj = cl.problem.applyConstraints (q1)
 
 q2 = [0.0, 0.0, 0.705, 1, 0, 0, 0, 0.0, 0.0, 0.0, 0.0, 1.0, 0, -1.4, -1.0, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.261799, -0.17453, 0.0, -0.523599, 0.0, 0.0, 0.174532, -0.174532, 0.174532, -0.174532, 0.174532, -0.174532, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0, 0.0, 0.0, -0.453786, 0.872665, -0.418879, 0.0]
 
-q2proj = cl.problem.createPositionConstraints (q2)
+#q2proj = cl.problem.createPositionConstraints (q2, -0.20, -0.30, 0.3)
+
+var1 = float(sys.argv[1])
+var2 = float(sys.argv[2])
+var3 = float(sys.argv[3])
+var4 = var1 + var2
+print 'vars=', var1, var2, var3, var4
+
+q2proj = cl.problem.createPositionConstraints (q2, var1, var2, var3)
 #cl.problem.setInitialConfig (q1proj)
 #cl.problem.addGoalConfig (q2proj)
 #cl.problem.solve ()
 
 r(q2proj)
+r(q2proj)
+r(q2proj)
+r(q2proj)
+r(q2proj)
 
 time.sleep(1)
-
 
 r(q2proj)
 
